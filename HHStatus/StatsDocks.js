911792e94edf45cb49069cbef13315c4a273a87f
NeKoT <47055042+NeKooT@users.noreply.github.com>
2023-12-09 01:15:52 +0100

    Create StatsDocks.js

---
+++ HHStatus/StatsDocks.js
@@ -0,0 +1,20 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+    EventName: Events.ClientReady,
+    async startAsync(client) {
+        setInterval (() => {
+            const guild = client.guilds.cache.get(process.env.GUILD)
+            const membres = guild.memberCount;
+            const online = guild.members.cache.filter(member => member.presence?.status === "online");
+            const dnd = guild.members.cache.filter(member => member.presence?.status === 'dnd');
+            const idle = guild.members.cache.filter(member => member.presence?.status === 'idle');
+
+            if(guild) {
+                guild.channels.cache.get('1180604259465699348').edit({name: `👥 Members: ${membres}`})
+                guild.channels.cache.get('1180608797820071946').edit({name: `🟢 ${online.size} ⛔ ${dnd.size} 🌙 ${idle.size}`})
+                guild.channels.cache.get('1180603973640659027').edit({name: `🔮 Boosts: ${guild.premiumSubscriptionCount}`})
+            }
+        }, 300000); 
+    },
+};
