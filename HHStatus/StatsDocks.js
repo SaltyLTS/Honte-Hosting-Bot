c185ae3aacedeafbd1086412b984891341f79686
Salty <52759884+Salty-Scripts@users.noreply.github.com>
2023-12-10 13:30:42 +0100

    Statdocks idle modifications
    Co-authored-by: NeKoT. <NeKooT@users.noreply.github.com>

--- HHStatus/StatsDocks.js
+++ HHStatus/StatsDocks.js
@@ -6,13 +6,14 @@ module.exports = {
         setInterval (() => {
             const guild = client.guilds.cache.get(process.env.GUILDID)
             const membres = guild.memberCount;
+            const offline = guild.members.cache.filter(member => member.presence?.status === "offline");
             const online = guild.members.cache.filter(member => member.presence?.status === "online");
             const dnd = guild.members.cache.filter(member => member.presence?.status === 'dnd');
             const idle = guild.members.cache.filter(member => member.presence?.status === 'idle');
 
             if(guild) {
                 guild.channels.cache.get('1180604259465699348').edit({name: `👥 Members: ${membres}`})
-                guild.channels.cache.get('1180608797820071946').edit({name: `🟢 ${online.size} ⛔ ${dnd.size} 🌙 ${idle.size + (guild.memberCount - online.size - dnd.size)}`})
+                guild.channels.cache.get('1180608797820071946').edit({name: `🟢 ${online.size} ⛔ ${dnd.size} 🌙 ${idle.size + offline.size}`})
                 guild.channels.cache.get('1180603973640659027').edit({name: `🔮 Boosts: ${guild.premiumSubscriptionCount}`})
             }
         }, 300000); 
