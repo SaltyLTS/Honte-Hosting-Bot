f15c9795757a7aeeba2a987ac5b031129c7926d2
Salty <52759884+Salty-Scripts@users.noreply.github.com>
2023-12-10 21:08:49 +0100

    Adding giveaway full integration
    Co-authored-by: NeKoT. <NeKooT@users.noreply.github.com>

---
+++ HHGiveaway/commands/drop-giveaway.js
@@ -0,0 +1,67 @@
+const { Events, EmbedBuilder, ApplicationCommandOptionType} = require('discord.js');
+const messages = require("../utils/messages");
+
+module.exports = {
+    name: "drop-giveaway",
+    description: 'Create a drop giveaway',
+    options: [
+        {
+            name: 'winners',
+            description: 'How many winners the giveaway should have',
+            type: ApplicationCommandOptionType.Integer,
+            required: true
+        },
+        {
+            name: 'prize',
+            description: 'What the prize of the giveaway should be',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: 'channel',
+            description: 'The channel to start the giveaway in',
+            type: ApplicationCommandOptionType.Channel,
+            required: true
+        }
+    ],
+    
+    EventName: Events.InteractionCreate,
+    async startAsync(interaction) {
+
+        // If the member doesn't have enough permissions
+        if(!interaction.member.permissions.has('MANAGE_MESSAGES') && !interaction.member.roles.cache.some((r) => r.name === "Giveaways")){
+            return interaction.reply({
+                content: ':x: You need to have the manage messages permissions to start giveaways.',
+                ephemeral: true
+            });
+        }
+    
+        const giveawayChannel = interaction.options.getChannel('channel');
+        const giveawayWinnerCount = interaction.options.getInteger('winners');
+        const giveawayPrize = interaction.options.getString('prize');
+    
+        if(!giveawayChannel.isTextBased()) {
+            return interaction.reply({
+                content: ':x: Selected channel is not text-based.',
+                ephemeral: true
+            });
+        }
+
+        // Start the giveaway
+        client.giveawaysManager.start(giveawayChannel, {
+            // The number of winners for this drop
+            winnerCount: giveawayWinnerCount,
+            // The prize of the giveaway
+            prize: giveawayPrize,
+            // Who hosts this giveaway
+            hostedBy: client.config.hostedBy ? interaction.user : null,
+            // specify drop
+            isDrop: true,
+            // Messages
+            messages
+        });
+    
+        interaction.reply(`Giveaway started in ${giveawayChannel}!`);
+
+    }
+};
