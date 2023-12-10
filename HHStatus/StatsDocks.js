b25cf70e094d31a4e38bfa8cc8ae9c57edae21bd
NeKoT <47055042+NeKooT@users.noreply.github.com>
2023-12-10 22:44:12 +0100

    Update StatsDocks.js

--- HHStatus/StatsDocks.js
+++ HHStatus/StatsDocks.js
@@ -6,16 +6,17 @@ module.exports = {
         setInterval (() => {
             const guild = client.guilds.cache.get(process.env.GUILDID)
             const membres = guild.memberCount;
-            const offline = guild.members.cache.filter(member => member.presence?.status == "offline");
-            const online = guild.members.cache.filter(member => member.presence?.status == "online");
-            const dnd = guild.members.cache.filter(member => member.presence?.status == 'dnd');
-            const idle = guild.members.cache.filter(member => member.presence?.status == 'idle');
+            const online = guild.members.cache.filter(member => ['online'].includes(member.presence?.status)).size;
+            const dnd = guild.members.cache.filter(member => ['dnd'].includes(member.presence?.status)).size;
+            const idle = guild.members.cache.filter(member => ['idle'].includes(member.presence?.status)).size;
+            const offline = guild.members.cache.filter(member => !['online', 'dnd', 'idle'].includes(member.presence?.status)).size;
+            const idleOff = (idle + offline);
 
             if(guild) {
                 guild.channels.cache.get('1180604259465699348').edit({name: `👥 Members: ${membres}`})
-                guild.channels.cache.get('1180608797820071946').edit({name: `🟢 ${online.size} ⛔ ${dnd.size} 🌙 ${idle.size + offline.size}`})
+                guild.channels.cache.get('1180608797820071946').edit({name: `🟢 ${online} ⛔ ${dnd} 🌙 ${idleOff}`})
                 guild.channels.cache.get('1180603973640659027').edit({name: `🔮 Boosts: ${guild.premiumSubscriptionCount}`})
             }
-        }, 300000); 
+        }, 5000); 
     },
 };
