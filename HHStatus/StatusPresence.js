aec005bae4f6e0067047d70ff143e193e4c50541
Salty <52759884+Salty-Scripts@users.noreply.github.com>
2023-12-23 19:31:45 +0100

    Update StatusPresence.js

--- HHStatus/StatusPresence.js
+++ HHStatus/StatusPresence.js
@@ -3,14 +3,29 @@ const { Events, ActivityType } = require('discord.js');
 module.exports = {
     EventName: Events.ClientReady,
     async startAsync(client) {
-                client.user.setPresence({
-                    activities: [
-                        {
-                            name: `Future best WW hosting !`,
-                            type: ActivityType.Custom
-                        }
-                    ],
-                    status: 'Online'
-        });
+        updateStatus(client);
+        setInterval(() => {
+            updateStatus(client);
+        }, 180000);
     },
 };
+
+function updateStatus(client) {
+    const statusMessages = [
+        'Future best WW hosting !',
+        'Opening soon !',
+        'I love you guys :3',
+    ];
+
+    const randomStatus = statusMessages[Math.floor(Math.random() * statusMessages.length)];
+
+    client.user.setPresence({
+        activities: [
+            {
+                name: randomStatus,
+                type: ActivityType.Custom
+            }
+        ],
+        status: 'online'
+    });
+}
